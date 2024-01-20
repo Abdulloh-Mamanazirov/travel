93af09d064b38ac5c0a0504e05708b78d4c85a91
Abdulloh <abdullajonmamnazirov@gmail.com>
2024-01-20 12:10:24 +0500

    edit carousel

--- src/pages/Home/components/showcase/index.jsx
+++ src/pages/Home/components/showcase/index.jsx
@@ -30,6 +30,25 @@ const index = () => {
         className="h-screen xs:h-[60vh] md:h-screen"
       >
         <SwiperSlide className="relative">
+          <div className="absolute inset-0 bg-black/20" />
+          <div className="absolute bottom-[20%] text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:w-1/2 p-3">
+            <h2 className="text-4xl sm:text-5xl md:text-7xl font-bold font-agbalumo">
+              Travel Uzbeksitan
+            </h2>
+            <p className="font-medium text-sm sm:text-base">
+              Travel across Uzbekistan. Visit the most popular, modern and, at
+              the same time historic places with us.
+            </p>
+          </div>
+          <img
+            src={Uzbekistan}
+            alt="Uzbekistan"
+            draggable={false}
+            className="select-none h-full w-screen object-cover"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="relative">
+          <div className="absolute inset-0 bg-black/20" />
           <div className="absolute bottom-[20%] text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:w-1/2 p-3">
             <h2 className="text-4xl sm:text-5xl md:text-7xl font-bold font-agbalumo">
               Dubai Skyline
@@ -47,6 +66,7 @@ const index = () => {
           />
         </SwiperSlide>
         <SwiperSlide className="relative">
+          <div className="absolute inset-0 bg-black/20" />
           <div className="absolute bottom-[20%] text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:w-1/2 p-3">
             <h2 className="text-4xl sm:text-5xl md:text-7xl font-bold font-agbalumo">
               Golden Mosque
@@ -65,6 +85,7 @@ const index = () => {
           />
         </SwiperSlide>
         <SwiperSlide className="relative">
+          <div className="absolute inset-0 bg-black/20" />
           <div className="absolute bottom-[20%] text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:w-1/2 p-3">
             <h2 className="text-4xl sm:text-5xl md:text-7xl font-bold font-agbalumo">
               Red square
@@ -83,6 +104,7 @@ const index = () => {
           />
         </SwiperSlide>
         <SwiperSlide className="relative">
+          <div className="absolute inset-0 bg-black/20" />
           <div className="absolute bottom-[20%] text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:w-1/2 p-3">
             <h2 className="text-4xl sm:text-5xl md:text-7xl font-bold font-agbalumo">
               Serene Bay View
@@ -100,6 +122,7 @@ const index = () => {
           />
         </SwiperSlide>
         <SwiperSlide className="relative">
+          <div className="absolute inset-0 bg-black/20" />
           <div className="absolute bottom-[20%] text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:w-1/2 p-3">
             <h2 className="text-4xl sm:text-5xl md:text-7xl font-bold font-agbalumo">
               Istanbul Skyline
@@ -113,23 +136,6 @@ const index = () => {
           <img
             src={Turkey}
             alt="Turkey"
-            draggable={false}
-            className="select-none h-full w-screen object-cover"
-          />
-        </SwiperSlide>
-        <SwiperSlide className="relative">
-          <div className="absolute bottom-[20%] text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] md:w-1/2 p-3">
-            <h2 className="text-4xl sm:text-5xl md:text-7xl font-bold font-agbalumo">
-              Travel Uzbeksitan
-            </h2>
-            <p className="font-medium text-sm sm:text-base">
-              Travel across Uzbekistan. Visit the most popular, modern and, at
-              the same time historic places with us.
-            </p>
-          </div>
-          <img
-            src={Uzbekistan}
-            alt="Uzbekistan"
             draggable={false}
             className="select-none h-full w-screen object-cover"
           />
